03a6b5cb94794eb338a564c127f2c04cb11b3e3f
Ryun Ji <ryunji1990@gmail.com>
2024-07-24 15:29:16 +0900

    Update ChatbotApplicationTests.java

--- src/test/java/com/AI/chatbot/ChatbotApplicationTests.java
+++ src/test/java/com/AI/chatbot/ChatbotApplicationTests.java
@@ -3,7 +3,7 @@ package com.AI.chatbot;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(classes = TakeItEasyApplicationTests.class)
+//@SpringBootTest(classes = TakeItEasyApplicationTests.class) : 24.07.24.화 grj 젠킨스 빌드 오류로 주석처리 함
 class ChatbotApplicationTests {
 
 	@Test
